184cdd377bae648d94affe91f725a84d1a8f0e1e
Gopikrishna P <138367605+Gopi004@users.noreply.github.com>
2026-05-17 06:35:54 +0000

    Added files

---
+++ ftp_c.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+
+int main(){
+    int sockfd;
+    struct sockaddr_in server;
+    char filename[1024],buffer[1024]={0};
+
+    sockfd=socket(AF_INET,SOCK_STREAM,0);
+    server.sin_family=AF_INET;
+    server.sin_addr.s_addr=inet_addr("127.0.0.1");
+    server.sin_port=htons(8080);
+
+    connect(sockfd,(struct sockaddr *)&server,sizeof(server));
+
+    printf("Enter the filename: ");
+    scanf("%s",&filename);
+
+    send(sockfd,filename,strlen(filename),0);
+
+    printf("File contents:\n");
+    while(read(sockfd,buffer,sizeof(buffer))>0){
+        printf("%s",buffer);
+        memset(buffer,0,sizeof(buffer));
+    }
+    close(sockfd);
+    return 0;
+}
